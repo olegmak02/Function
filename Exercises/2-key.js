deb4337bccc44b60c03eda82031c473d051bd551
olegmak02 <olleglion02@gmail.com>
2020-01-04 01:28:45 +0200

    commit

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  let amount = possible.length;
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * amount);
+    key += possible[index];
+  }
+  return key;
 };
 
 module.exports = { generateKey };
